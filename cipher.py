ed017feddb2d72fcdf1554f9a569944e859aaf0f
tonyanis <94333436+tonyanis@users.noreply.github.com>
2021-12-02 16:59:57 -0500

    Update cipher.py

--- cipher.py
+++ cipher.py
@@ -5,8 +5,6 @@ import time #SR needed to use time.time() function
 SYMBOLS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
 MAX_KEY_SIZE = len(SYMBOLS)
 
-#GA I combined the functions that SS made to determine how the game will be played and the time function made by SR to make the game work, and the final if statement in time function to check if input is correct
-
 #SS lists that correspond to level of difficulty, TA created words that go inside of lists
 level_easy = ['elf', 'bell', 'gift', 'ice', 'toys', 'log', 'hope', 'snow', 'cold', 'pie', 'star', 'wish', 'noel', 'joy', 'love', 'coal', 'tree', 'red', 'gold']
 level_medium = ['angel', 'green', 'candy', 'merry', 'dasher', 'candle', 'dancer', 'frosty', 'socks', 'carols', 'jolly', 'ribbon', 'light', 'sleigh', 'vixon', 'holly', 'cookie', 'santa', 'family', 'donner', 'winter', 'cupid', 'comet', 'parade']
@@ -14,6 +12,14 @@ level_hard = ['rudolph', 'prancer', 'holiday', 'blitzen', 'northpole', 'polarbea
 
 #SS This function will ask user to input level of difficulty
 def how_difficult(): 
+    '''(str)->str
+    This function allows the player to enter the level of difficulty of choice.
+        Each diffuclty is ranked by the number of letters in the word, starting with Easy (3-4 letters), Medium (5-6 letters) and Hard (7+ letters)
+        
+>>>how_difficult("easy")
+>>>how_difficult("medium")
+>>>how_difficult("hard")
+    '''    
     while True:
         print("Enter level of difficulty: easy, medium or hard")
         difficulty = input()
@@ -24,6 +30,9 @@ def how_difficult():
                
 #SS This function chooses a random word from the lists based on difficulty
 def word_to_be_encrypted():
+    '''This function picks a random word from the list of words regarding the difficulty selected by the player.
+        This will be the word the player has to decrypt.
+    '''    
     if difficulty == 'easy': #SS If the how_difficult function returns easy
         r_word = random.choice(level_easy) #SS chooses a random word from level_easy
         return r_word
@@ -36,6 +45,14 @@ def word_to_be_encrypted():
     
 #SS This function asks user to input a key to encrypt the message
 def encryption_key():
+    '''(int)->str
+    This function lets you pick the number that your alphabet will be shifted by. This alphabet includes upper case letters followed by lower case letters.
+        Therefore, if the player picks the number 3, the entire alpabet will be shifted to the right by 3 letters, (The letter A will become D, B will become E, G will become J, etc.)
+ 
+>>>encryption_key(9)
+>>>encryption_key(32)
+>>>encryption_key(46)
+    '''
     while True:
         key = int(input("Please enter a number between 1 and 52\n")) #SS Changes the input to type int
         if (key >=1 and key <= MAX_KEY_SIZE): #SS Checking to make sure the key is between 1 and 52
@@ -45,6 +62,14 @@ def encryption_key():
             
 #SR This function asks the user to input how long they'd like the game to last for
 def get_delay():
+    '''(int)->str
+    This function lets the player pick the lenght of the game. 
+        This is a way the player can challenge themselves by decrypting as many words as possible in the shortest time.
+
+>>>get_delay(30) 
+>>>get_delay(60)
+>>>get_delay(90)
+    '''           
     while True:
         delay = int(input("Game Time: 30, 60, 90 seconds?\n"))#input time
         if delay == int(30):
@@ -58,11 +83,17 @@ def get_delay():
          
 #GA Find the total time of the game using the delay found above 
 def gameTime(delay):
+    ''' This function determines when the game is finised. 
+            The lenght of the game is determined by the player and once the time is finished, the game will end.
+    '''
     game_end = time.time() + delay
     return game_end
 
 #SS Encrypts the r_word, the word that was chosen randomly
 def getTranslatedMessage(r_word, key):
+    '''(str)->str
+    This function will allow the player to decrypt the next word. The new word will then be displayed for the player to decrypt. 
+    '''
     new_word="" #SS empty string to add the encrypted symbols    
     for symbol in r_word: 
         symbolIndex = SYMBOLS.find(symbol)
@@ -98,7 +129,7 @@ while True:
         print(message) #SS Prints the result of the encryption
         guess = input("Enter what you think the decrypted word is (your key of incryption is " + str(key) + "):\n")
         if guess == r_word:
-            print("You are correct")
+            print("You are correct! Your new word is:")
             r_word = word_to_be_encrypted()
             message = getTranslatedMessage(r_word, key)
         else:
